94fe217e4c70e907904b4a2b195606659b2d1f37
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-02 15:52:46 -0600

    Update create food spec to include new food serializer format.

--- spec/requests/api/v1/create_food_spec.rb
+++ spec/requests/api/v1/create_food_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe 'Food API' do
-  it 'allows use to create a new food with name and calories' do
+  it 'allows user to create a new food with name and calories' do
     params = {name: 'banana', calories: '10'}
     post '/api/v1/foods', params: params
 
     result = JSON.parse(response.body)
 
     expect(result).to be_a Hash
-    expect(result["name"]).to eq('banana')
-    expect(result["calories"]).to eq(10)
+    expect(result["food"]["name"]).to eq('banana')
+    expect(result["food"]["calories"]).to eq(10)
   end
 
   it "returns 400 if food is not saved" do
