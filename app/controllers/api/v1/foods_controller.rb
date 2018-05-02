d214b1f6dcbd1db880d1f8cced3d952cc4606a8a
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-02 14:38:26 -0600

    Add show action to food.

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -3,4 +3,12 @@ class Api::V1::FoodsController < ApplicationController
   def index
     render json: Food.all
   end
+
+  def show
+    begin
+      render json: Food.find(params[:id])
+    rescue
+      render status: 404
+    end
+  end
 end
