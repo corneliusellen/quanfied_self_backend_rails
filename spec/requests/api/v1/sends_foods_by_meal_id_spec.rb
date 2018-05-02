48c37a0cbc95b0dcf719bb14409c397c3a9beca6
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-02 17:57:11 -0600

    Create test for sending foods associated with meal id.

---
+++ spec/requests/api/v1/sends_foods_by_meal_id_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Meals API" do
+  it "sends all foods associated with meal id" do
+    fruit = create(:food)
+    veggie = create(:food)
+    breakfast = create(:meal)
+    breakfast.foods << [fruit, veggie]
+
+    get "/api/v1/meals/#{breakfast.id}/foods"
+
+    result = JSON.parse(response.body)
+
+    expect(result).to be_a Hash
+    expect(result["name"]).to eq(breakfast.name)
+    expect(result["foods"].count).to eq(2)
+    expect(result["foods"][0]["name"]).to eq(fruit.name)
+    expect(result["foods"][0]["calories"]).to eq(fruit.calories)
+  end
+end
