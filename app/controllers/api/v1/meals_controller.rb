da2337acc326780d23f0b1a4fe23990237ec5300
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-02 18:15:40 -0600

    Add create action to meals controller.

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -13,6 +13,14 @@ class Api::V1::MealsController < ApplicationController
   end
 
   def create
+    begin
+      meal = Meal.find(params[:meal_id])
+      food = Food.find(params[:food_id])
+      meal.foods << food
+      render json: {message: "Successfully added #{food.name} to #{meal.name}"}
+    rescue
+      render status: 404
+    end 
   end
 
   def destroy
